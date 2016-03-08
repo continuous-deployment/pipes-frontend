f80149132dd79f2abe9fa5f3fe58fb4787482594
Chris Normansell <chris.normansell@gmail.com>
2016-03-08 23:38:42 +0000

    Updated projects view to pull in from localhost:8000

--- app/views/Projects.js
+++ app/views/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 
 class Projects extends Component
 {
@@ -6,11 +7,39 @@ class Projects extends Component
     items: PropTypes.array
   }
 
+  state = {
+    projects: []
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: 'http://localhost:8000/api/v1/projects',
+      type: 'GET',
+      dataType: 'JSON',
+      success: function (data) {
+        this.setState({ projects: data.data });
+      }.bind(this)
+    });
+  }
+
   render () {
+    let projects = this.state.projects.map(function (project, index) {
+        let url = 'projects/' + project.id;
+        return <Link
+          to={url}
+          key={project.id}
+          className="list-group-item"
+        >
+          {project.attributes.name} / {project.attributes.group}
+        </Link>;
+    })
+
     return (
       <div>
         <h2>Projects</h2>
-        {this.props.items || 'This will be a list of projects'}
+        <div class="list-group">
+          {projects}
+        </div>
       </div>
     );
   }
