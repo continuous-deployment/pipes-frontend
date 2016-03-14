f00f95a05ca893fcad946ef6c485c2d50a05ea4f
Chris Normansell <chris.normansell@gmail.com>
2016-03-14 22:43:15 +0000

    Removed key for now as it was causing problems

--- app/components/pipeline/PipelineRow.js
+++ app/components/pipeline/PipelineRow.js
@@ -21,7 +21,7 @@ class PipelineRow extends Component
         return '';
       }
 
-      return <div className={'col-sm-' + pipe.size} key={pipe.pipe.id + '_container'}>
+      return <div className={'col-sm-' + pipe.size}>
         <PipeType
           pipe={pipe.pipe}
           {...this.props}
