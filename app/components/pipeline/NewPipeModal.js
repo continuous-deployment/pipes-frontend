788f7d8ec7c35d6ef67b0a6258fd6c8760d4dd51
Chris Normansell <chris.normansell@gmail.com>
2016-03-14 22:42:25 +0000

    Refactored new pipe modal to work with the store and only including once per view

--- app/components/pipeline/NewPipeModal.js
+++ app/components/pipeline/NewPipeModal.js
@@ -9,7 +9,7 @@ import ActionForm from './pipe-forms/ActionForm.js';
 class NewPipeModal extends Component
 {
   static propTypes = {
-    modalId: PropTypes.string,
+    show: PropTypes.bool,
     relationship: PropTypes.string,
     pipeId: PropTypes.string
   }
@@ -19,17 +19,33 @@ class NewPipeModal extends Component
     attributes: {}
   }
 
+  componentDidUpdate () {
+    this.showModal();
+  }
+
+  componentDidMount () {
+    this.showModal();
+  }
+
+  showModal() {
+    if (this.props.show) {
+      window.$('#newPipeModal').modal('show');
+    } else {
+      window.$('#newPipeModal').modal('hide');
+    }
+  }
+
   render () {
     var form = this.getFormByType(this.state.pipe_type);
 
-    return <div className='modal fade' tabIndex='-1' role='dialog' id={this.props.modalId}>
+    return <div className='modal fade' tabIndex='-1' role='dialog' id='newPipeModal'>
       <div className='modal-dialog'>
         <div className='modal-content'>
           <div className='modal-header'>
             <button
               type='button'
               className='close'
-              data-dismiss='modal'
+              onClick={this.handleHideModal.bind(this)}
               aria-label='Close'
             >
               <span aria-hidden='true'>&times;</span>
@@ -57,14 +73,13 @@ class NewPipeModal extends Component
             <button
               type='button'
               className='btn btn-danger'
-              data-dismiss='modal'
+              onClick={this.handleHideModal.bind(this)}
             >
               Cancel
             </button>
             <button
               type='button'
               className='btn btn-success'
-              data-dismiss='modal'
               onClick={this.handleAdd.bind(this)}
             >
               Add
@@ -113,6 +128,16 @@ class NewPipeModal extends Component
         attributes: this.state.attributes,
         relationship: this.props.relationship,
         parentPipeId: this.props.pipeId
+      }
+    );
+    this.handleHideModal();
+  }
+
+  handleHideModal () {
+    Dispatcher.handleViewAction(
+      'SHOW_MODAL',
+      {
+        show: false,
       }
     );
   }
